4eda6012fb9c1d8af49a721de650ee1230083450
Giovanni <gio.a.98@gmail.com>
2021-11-15 13:31:57 +0100

    fine snack 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-// +++ JSnack 1 +++
-let somma = 0;
+// // +++ JSnack 1 +++
+
+// let somma = 0;
 // // for
 // for (let i = 0; i < 5; i++) {
 //     somma = somma + parseInt(prompt("Inserisci un numero da sommare:"));
 // }
-// while
-let i = 0;
-while (i < 5) {
-    somma = somma + parseInt(prompt("Inserisci un numero da sommare:"));
-    i++;
+// // while
+// let i = 0;
+// while (i < 5) {
+//     somma = somma + parseInt(prompt("Inserisci un numero da sommare:"));
+//     i++;
+// }
+// console.log("La somma è: " + somma);
+
+
+// +++ JSnack 2 +++
+
+const word1 = prompt("Inserisci la prima parola: ");
+const word2 = prompt("Inserisci la seconda parola: ");
+if (word1.length > word2.length) {
+    console.log(word1);
+} else if (word1.length < word2.length) {
+    console.log(word2);
+} else {
+    console.log(word1 + " " + word2);
 }
-console.log("La somma è: " + somma);
